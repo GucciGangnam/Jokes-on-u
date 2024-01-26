82e9e8442b4c5a72ccb12ff2c5602a34919266d9
Gucci Gangnam <diviner02hive@icloud.com>
2024-01-26 13:46:12 +0700

    Tweaking finals

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -22,12 +22,14 @@ exports.account_get = asyncHandler(async (req, res, next) => {
 exports.code_post = asyncHandler(async (req, res, next) => {
     const user = req.user;
     const submission = req.body.code;
-    console.log(submission)
+    console.log(`(${submission} == 42) = ${submission == 42}`); // Log the logic result
+
     if (submission == 42) { 
         // set user.VIP true
         await Users.updateOne({ _id: req.user.id }, { $set: { VIP: true } });
         res.redirect("/account")
     } else { 
+        console.log((submission == 42))
         res.render("account", {user:user, correct:false})
     }
 })
